a12fcc1923cce9378cd78a9ed6a5f2ba664efa56
Faezeh Khadem <73037085+Faezeh-khd@users.noreply.github.com>
2022-06-10 18:44:19 +0430

    Add files via upload
    adding files

---
+++ corenlp_examaple/corenlp_my_examaple/src/main/java/CoreNlpExample.java
@@ -0,0 +1,49 @@
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.util.List;
+import java.util.Properties;
+
+public class CoreNlpExample {
+    public static void main(String[] args) {
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        // build pipeline
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "Joe Smith was born in California. " +
+                "In 2017, he went to Paris, France in the summer. " +
+                "His flight left at 3:00pm on July 10th, 2017. " +
+                "After eating some escargot for the first time, Joe said, \"That was delicious!\" " +
+                "He sent a postcard to his sister Jane Smith. " +
+                "After hearing about Joe's trip, Jane decided she might go to France one day.";
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+                System.out.println(String.format("Print: word: [%s] pos: [%s] ne: [%s]", word, pos, ne));
+            }
+        }
+    }
+}
